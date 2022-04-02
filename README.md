f4d54bd63cc5bbcba33e554b27bc9c8e069f0300
amirgamil <amir.amgad01@gmail.com>
2022-04-02 12:36:54 -0400

    update README and add export script

--- README.md
+++ README.md
@@ -6,11 +6,15 @@ If you use [Curius](https://curius.app/), you can get a version of this running 
 
 ## Running with your Curius
 
-1. Run `cp .env.example .env` to setup your .env file. In your `.env` file, put the value of your Curius ID. You can do this by opening up `Inspect Element` in Chrome, then navigate to your curius bookshelf i.e. `https://curius.app/<your username>`. In the network tab, you should see a request made that has the URL `https://curius.app/api/user`. Open up the response and you should see your ID there. 
-![Screen Shot 2022-04-02 at 2 10 07 AM](https://user-images.githubusercontent.com/7995105/161369616-10474b17-d978-432a-9f31-9fa58f9611a0.png)
-![Screen Shot 2022-04-02 at 2 10 41 AM](https://user-images.githubusercontent.com/7995105/161369618-fbcb21fa-07ee-47c6-90e6-68c3ea92c8f2.png)
+1. Run `cp .env.example .env` to setup your .env file. In your `.env` file, put the value of your Curius ID. You can do this by opening up `Inspect Element` in Chrome, then navigate to your curius bookshelf i.e. `https://curius.app/<your username>`. In the network tab, you should see a request made that has the URL `https://curius.app/api/user`. Open up the response and you should see your ID there.
+   ![Screen Shot 2022-04-02 at 2 10 07 AM](https://user-images.githubusercontent.com/7995105/161369616-10474b17-d978-432a-9f31-9fa58f9611a0.png)
+   ![Screen Shot 2022-04-02 at 2 10 41 AM](https://user-images.githubusercontent.com/7995105/161369618-fbcb21fa-07ee-47c6-90e6-68c3ea92c8f2.png)
 
 2. Run `go run cmd/apollo-curius.go`
 3. You should be set! Wait maybe a minute or two for the program to fetch all your Curius data and set up the inverted index, when you see a `Server listening on 0.0.0.0:8990`, you can navigate to `http://localhost:8900` and it should be working!
 
 Note if you want to deploy it, this might be a little more involved, but I can help if you DM me on [Twitter](https://twitter.com/amirbolous). I have a Digital Ocean Droplet and I deployed this app as a `systemctl` service behind an `nginx` reverse-proxy. If none of that makes sense, DM me and I'll explain.
+
+## Exporting your Curius
+
+If you want to export all of your curius data, you can also run `go run cmd/apollo-curius.go --export` which should create a file `curius.json` inside the data folder
--- cmd/apollo-curius.go
+++ cmd/apollo-curius.go
@@ -2,38 +2,45 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"github.com/amirgamil/curius-search/pkg/apollo-curius"
 	"github.com/amirgamil/curius-search/pkg/apollo-curius/backend"
+	"github.com/amirgamil/curius-search/pkg/curius"
 )
 
 func main() {
 	// sources.ReadXMLFile()
-	backend.InitializeFilesAndData()
-	//we call ticker to refresh inverted index regularly once every 3 days however
-	//for convenience we often want to do a refresh "on start" so we add this here too
-	backend.RefreshInvertedIndex()
-	log.Println("Refreshing inverted index on launch: ")
-	// two days in miliseconds
-	// once every three days, takes all the records, pulls from the data sources,
-	ticker := time.NewTicker(3 * 24 * time.Hour)
-	done := make(chan bool)
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case t := <-ticker.C:
-				log.Println("Refreshing inverted index at: ", t)
-				backend.RefreshInvertedIndex()
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) != 0 && argsWithoutProg[0] == "--export" {
+		curius.SaveCuriusArticles()
+	} else {
+		backend.InitializeFilesAndData()
+		//we call ticker to refresh inverted index regularly once every 3 days however
+		//for convenience we often want to do a refresh "on start" so we add this here too
+		backend.RefreshInvertedIndex()
+		log.Println("Refreshing inverted index on launch: ")
+		// two days in miliseconds
+		// once every three days, takes all the records, pulls from the data sources,
+		ticker := time.NewTicker(3 * 24 * time.Hour)
+		done := make(chan bool)
+		go func() {
+			for {
+				select {
+				case <-done:
+					return
+				case t := <-ticker.C:
+					log.Println("Refreshing inverted index at: ", t)
+					backend.RefreshInvertedIndex()
+				}
 			}
-		}
-	}()
-	//server and the pipeline should run on concurrent threads, called regularly, for now manually do it
-	//start the server on a concurrent thread so that when we need to refresh the inverted index, this happens on
-	//different threads
-	// backend.RefreshInvertedIndex()
-	apollo.Start()
+		}()
+		//server and the pipeline should run on concurrent threads, called regularly, for now manually do it
+		//start the server on a concurrent thread so that when we need to refresh the inverted index, this happens on
+		//different threads
+		// backend.RefreshInvertedIndex()
+		apollo.Start()
+	}
 
 }
